bb52d21f1c8516567f07afaa037ee87ee05a6a88
John-Goo <guhaijiang666@hotmail.com>
2020-12-19 17:16:28 +0800

    初始化项目

---
+++ springboot-common/src/main/java/com/wit2cloud/springboot/common/exception/Demo.java
@@ -0,0 +1,16 @@
+package com.wit2cloud.springboot.common.exception;
+/*==========================================================================
+ * Copyright (C) Wit2Cloud Co.,Ltd
+ * All Rights Reserved.
+ * Created By 开源学社
+ ==========================================================================*/
+
+/**
+ * @author John Goo
+ * @version 1.0
+ * @ClassName: Demo
+ * @Desc: TODO
+ * @history v1.0
+ */
+public class Demo {
+}
---
+++ springboot-util/src/main/java/com/wit2cloud/springboot/util/Demo.java
@@ -0,0 +1,19 @@
+package com.wit2cloud.springboot.util;
+/*==========================================================================
+ * Copyright (C) Wit2Cloud Co.,Ltd
+ * All Rights Reserved.
+ * Created By 开源学社
+ ==========================================================================*/
+
+/**
+ * @author John Goo
+ * @version 1.0
+ * @ClassName: Demo
+ * @Desc: TODO
+ * @history v1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
